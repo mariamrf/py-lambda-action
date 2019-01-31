3278fac50e9a8289c6addf2a44fc1b7dd8f5ae6f
Mariam Maarouf <mariam@door2door.io>
2019-01-31 17:29:21 +0100

    Add action code

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.6
+
+LABEL "com.github.actions.name"="Py Lambda Deploy"
+LABEL "com.github.actions.description"="Deploy python code to Lambda with dependencies in a separate layer."
+LABEL "com.github.actions.icon"="layers"
+LABEL "com.github.actions.color"="yellow"
+
+LABEL "repository"="http://github.com/mariamrf/py-lambda-action"
+LABEL "maintainer"="Mariam Maarouf <mrf.mariam@gmail.com>"
+
+RUN apt-get update
+RUN apt-get install -y jq zip
+RUN pip install awscli
+
+ADD entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+check_if_branch(){
+	local ref=$(jq '.ref' $GITHUB_EVENT_PATH)
+	if ! [[ -z ${BRANCH} ]]; then
+		if [ "$ref" != "\"refs/heads/$BRANCH\"" ]; then
+			echo "Not deploying because we're not on $BRANCH"
+			exit 0
+		fi
+	fi
+}
+
+install_zip_dependencies(){
+	echo "Installing and zipping dependencies..."
+	mkdir python
+	pip install --target=python -r requirements.txt
+	zip -r dependencies.zip ./python
+}
+
+publish_dependencies_as_layer(){
+	echo "Publishing dependencies as a layer..."
+	local result=$(aws lambda publish-layer-version --layer-name "${LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+	LAYER_VERSION=$(jq '.Version' <<< "$result")
+	rm -rf dependencies
+	rm dependencies.zip
+}
+
+publish_function_code(){
+	echo "Deploying the code itself..."
+	zip -r code.zip . -x \*.git\*
+	aws lambda update-function-code --function-name "${LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+}
+
+update_function_layers(){
+	echo "Using the layer in the function..."
+	aws lambda update-function-configuration --function-name "${LAMBDA_FUNCTION_NAME}" --layers "${LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+}
+
+deploy_lambda_function(){
+	install_zip_dependencies
+	publish_dependencies_as_layer
+	publish_function_code
+	update_function_layers
+}
+
+check_if_branch
+deploy_lambda_function
+echo "Done."
