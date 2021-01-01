ed999781cad2add8153cad02498fa7f716d81de1
Josh Rosenberg <joshua_rosenberg@comcast.com>
2021-01-01 14:27:06 -0500

    add option to upload dependency layer to S3 first

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,9 +23,18 @@ install_zip_dependencies(){
 }
 
 publish_dependencies_as_layer(){
-	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
-	LAYER_VERSION=$(jq '.Version' <<< "$result")
+	if [ "$INPUT_USE_S3" = true ]
+	then
+		echo "Uploading dependencies to S3..."
+		aws s3 cp dependencies.zip s3://"${INPUT_S3_BUCKET_NAME}"/dependencies.zip
+		echo "Publishing dependencies from S3 as a layer..."
+		local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --content S3Bucket="${INPUT_S3_BUCKET_NAME}",S3Key=dependencies.zip)
+		LAYER_VERSION=$(jq '.Version' <<< "$result")
+	else
+		echo "Publishing dependencies as a layer..."
+		local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+		LAYER_VERSION=$(jq '.Version' <<< "$result")
+	fi
 	rm -rf python
 	rm dependencies.zip
 }
