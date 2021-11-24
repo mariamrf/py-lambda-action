27173b29013165709c578559a5ea2c41c876957d
Hervé Lee <hervelee@gmail.com>
2021-11-24 09:44:13 +0100

    try again

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,7 +47,7 @@ make_archive()
     if [ -z "$INPUT_EXCLUDES" ]; then
 	zip_opts=
     else
-	zip_opts="-x $(for w in $INPUT_EXCLUDES; do echo ${w/\*/\\\\*}; done |xargs)"
+	zip_opts="-x $(sed -e s#\*#\\\\*#g <<< $INPUT_EXCLUDES)"
     fi
     debug "INPUT_EXCLUDES: $INPUT_EXCLUDES"
     debug "zip_opts: $zip_opts"
