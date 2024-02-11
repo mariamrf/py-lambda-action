a6370f24909c3bdb620e2906f7d3ec022357578e
Nic Jones <nic@nicpjones.com>
2024-02-11 12:45:50 -0500

    fix: wait state

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,13 +25,16 @@ publish_function_code(){
 update_function_layers(){
 	echo "Using the layer in the function..."
 	local function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
-	echo "The Function Status is: $function_state"
-	while [[ $function_state != "\"Active\"" ]]; do
+ 	local function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
+	echo "The Function State is: $function_state"
+ 	echo "The Function Status is: $function_status"
+	while [[ $function_state != "\"Active\"" && $function_status != "\"Successful\""]]; do
 		function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
-		echo "The Function Status is: ${function_state}"
-		sleep 5
+  		function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
+		echo "The Function State is: $function_state"
+ 		echo "The Function Status is: $function_status"
+		sleep 1
 	done
- 	echo "The Function Status is: ${function_state}"
 	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
 }
 
