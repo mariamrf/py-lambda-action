3c6413c4aa56f7307ed91e4458ee11e9fe332ba8
Nic Jones <nic@nicpjones.com>
2024-02-11 12:53:33 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,8 @@ install_zip_dependencies(){
 
 publish_dependencies_as_layer(){
 	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+ 	local result
+	result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
 	rm -rf python
 	rm dependencies.zip
@@ -24,18 +25,18 @@ publish_function_code(){
 
 update_function_layers(){
 	echo "Using the layer in the function..."
-	local function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
- 	local function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
-	echo "The Function State is: $function_state"
- 	echo "The Function Status is: $function_status"
+ 	local function_state
+  	local function_status
+	function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
+ 	function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
 	while [[ $function_state != "\"Active\"" && $function_status != "\"Successful\"" ]]
  	do
 		function_state=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.State')
   		function_status=$(aws lambda get-function --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --query 'Configuration.LastUpdateStatus')
-		echo "The Function State is: $function_state"
- 		echo "The Function Status is: $function_status"
 		sleep 1
 	done
+ 	echo "The Function State is: $function_state"
+ 	echo "The Function Status is: $function_status"
 	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
 }
 
